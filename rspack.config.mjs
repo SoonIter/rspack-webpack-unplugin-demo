2dea60e05644bb8e3a32ccdd8a244507635e2997
sunyiteng <sunyiteng@bytedance.com>
2024-12-04 17:03:39 +0800

    chore: update

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,9 +1,4 @@
-import path from 'path';
 import { rspackPlugin } from './unplugin-coolson.mjs';
-
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default {
   entry: './src/index.mjs',
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,9 +1,4 @@
-import path from 'path';
 import { webpackPlugin } from './unplugin-coolson.mjs';
-
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default {
   entry: './src/index.mjs',
