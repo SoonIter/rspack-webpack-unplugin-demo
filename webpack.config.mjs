c72200b51b4b253c66d09a91927fd6ad018046c8
sunyiteng <sunyiteng@bytedance.com>
2024-12-05 15:21:10 +0800

    chore: update

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,6 +1,52 @@
-import { webpackPlugin } from './unplugin-coolson.mjs';
+// import { rspackPlugin } from './unplugin-coolson.mjs';
+import { join } from 'path';
+const __dirname = new URL('.', import.meta.url).pathname;
 
 export default {
-  entry: './src/index.mjs',
-  plugins: [webpackPlugin()],
+  mode: 'production',
+  context: __dirname,
+  entry: {
+    main: [join(__dirname, './src/main.mjs')],
+    'assets/react': [join(__dirname, './src/assets/react.svg')],
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.tsx', '.jsx'],
+  },
+  externals: [
+    // ({ request, dependencyType, contextInfo }, callback) => {
+    //   if (request?.[0] === '.') {
+    //     return callback(undefined, request);
+    //   }
+    // },
+  ],
+  stats: 'verbose',
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    publicPath: '/',
+    module: true,
+    chunkFormat: 'module',
+    library: {
+      type: 'modern-module',
+    },
+    chunkLoading: 'import',
+    workerChunkLoading: 'import',
+    wasmLoading: 'fetch',
+  },
+
+  externalsType: 'module-import',
+  experiments: {
+    css: true,
+    asyncWebAssembly: true,
+    outputModule: true,
+  },
+  optimization: {
+    minimize: false,
+  }
 };
